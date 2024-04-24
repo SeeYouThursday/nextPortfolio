3ca4287c000777b3f4c7b8f084d7e0fdd505ee35
Brian <galyenb@vcu.edu>
2024-04-24 12:16:03 -0400

    resume route bug fixed/added back download/link btns TODO: change btn text

--- src/app/document-resume/page.tsx
+++ src/app/document-resume/page.tsx
@@ -3,27 +3,27 @@ import DownloadBtn from '../_components/Btns/DownloadBtn';
 import ResumeLinkBtn from '../_components/Btns/ResumeLinkBtn';
 
 const Resume = () => {
-  // const proficencies = [
-  //   'Agile Development',
-  //   'Full Stack Development',
-  //   'React',
-  //   'MERN',
-  //   'MongoDB',
-  //   'JavaScript',
-  //   'CSS',
-  //   'Node',
-  //   'Nextjs',
-  //   'GraphQL',
-  //   'RESTful APIs',
-  // ];
+  const proficencies = [
+    'Agile Development',
+    'Full Stack Development',
+    'React',
+    'MERN',
+    'MongoDB',
+    'JavaScript',
+    'CSS',
+    'Node',
+    'Nextjs',
+    'GraphQL',
+    'RESTful APIs',
+  ];
 
   return (
     <div className="m-auto">
       <h3 className="text-4xl">Proficiencies:</h3>
-      {/* <SkillsCard skills={proficencies} /> */}
+      <SkillsCard skills={proficencies} />
       <div className="flex justify-between mt-4">
-        {/* <DownloadBtn />
-        <ResumeLinkBtn /> */}
+        <DownloadBtn />
+        <ResumeLinkBtn />
       </div>
     </div>
   );
